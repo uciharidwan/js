0952d719530f3b4a7eb59a9f29d6e0f88100fccf
uciharidwan <161241056+uciharidwan@users.noreply.github.com>
2024-02-26 01:27:10 +0700

    Update igbiru.js

--- igbiru.js
+++ igbiru.js
@@ -1,19 +1,32 @@
-function kirimPesanTelegram(pesan) {
-    var botToken = '6013753018:AAEWqmIp8DmwejWRqbC0MpL1QaN-zJdopCE'; // Token bot Anda
-    var chatId = '2079666452'; // ID obrolan (chat ID) Anda
-    var message = encodeURIComponent(pesan);
-    var url = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + chatId + '&text=' + message;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
-}
+<script>
+    // Fungsi untuk mengirim pesan ke Telegram
+    function kirimPesanTelegram(pesan) {
+        var botToken = '6013753018:AAEWqmIp8DmwejWRqbC0MpL1QaN-zJdopCE'; // Ganti dengan token bot Anda
+        var chatId = '2079666452'; // Ganti dengan ID obrolan Anda
+        var message = encodeURIComponent(pesan);
+        var url = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + chatId + '&text=' + message;
 
-// Menangani pengiriman form
-document.getElementById('form-pesan').addEventListener('submit', function(event) {
-    event.preventDefault(); // Menghentikan pengiriman form bawaan
-    var pesan = document.getElementById('pesan').value;
-    kirimPesanTelegram(pesan);
-    // Tambahkan logika lain yang Anda perlukan setelah pengiriman pesan
-});
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // Handle response if needed
+                console.log(data);
+            })
+            .catch(error => {
+                // Handle error if needed
+                console.error('Error:', error);
+            });
+    }
+
+    // Menangani pengiriman form
+    document.querySelector('.contacts__form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Menghentikan pengiriman form bawaan
+
+        var pesan = 'Pesan yang ingin Anda kirim'; // Sesuaikan pesan sesuai dengan data dari form
+        kirimPesanTelegram(pesan);
+
+        // Tambahkan logika lain yang Anda perlukan setelah pengiriman pesan
+    });
+</script>
+                                                               
